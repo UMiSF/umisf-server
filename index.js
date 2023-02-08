b87509426830ec758be71ba94140b8fceda340c9
poornaSavindi <89610997+poornaSavindi@users.noreply.github.com>
2023-02-09 05:18:26 +0530

    Initialize files

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+require('dotenv').config();
+const app = express();
+const cors = require("cors");
+const mongoose = require('mongoose');
+
+const api = process.env.API_URL;
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+//routers
+
+
+//middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(cors());
+app.options('*',cors());
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3001;
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+    dbName: process.env.DATABASE
+}).then(()=>{
+    console.log("Database connection is ready.")
+}).catch((err)=>{
+    console.log(err);
+})
+
+app.listen(PORT, ()=>{
+    console.log("Listening to port number" + PORT)
+})
