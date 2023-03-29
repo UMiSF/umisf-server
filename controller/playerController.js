e78f9012d6b49552dde9ae2c15f6f46d6c4d3ddf
Nethmi Jayakody <nethmi.19@cse.mrt.ac.lk>
2023-03-29 20:28:47 +0530

    chnages to player retrieve by id

--- controller/playerController.js
+++ controller/playerController.js
@@ -1,41 +1,31 @@
-const databaseWrapper = require('../database/databaseWrapper');
+const databaseWrapper = require("../database/databaseWrapper");
 
 const addPlayer = async (req, res) => {
   const { playerData } = req.body;
   for (let i = 0; i < playerData.length; i++) {
     const player = playerData[i];
-    if (
-      !player.firstName ||
-      !player.lastName ||
-      !player.institute ||
-      !player.performanceThreshold ||
-      !player.gender ||
-      !player.contactNumber ||
-      !player.photo
-    ) {
-      return res.status(400).send({ message: 'required data not filled' });
+    if (!player.firstName || !player.lastName || !player.institute || !player.performanceThreshold || !player.gender || !player.contactNumber || !player.photo) {
+      return res.status(400).send({ message: "required data not filled" });
     }
   }
 
-  return await databaseWrapper.add('player', playerData, res);
-}; 
+  return await databaseWrapper.add("player", playerData, res);
+};
 
 const getAllPlayers = async (req, res) => {
-  const result =  await databaseWrapper.read('player', res);
-    res
-      .status(201)
-      .send({ message: result.message, data: result.data });
+  const result = await databaseWrapper.read("player", res);
+  res.status(201).send({ message: result.message, data: result.data });
 };
 
 const deleteByField = async (req, res) => {
   try {
     const { field, value } = req?.params;
     if (!field || !value) {
-      return res.status(400).send({ message: 'required data not filled' });
+      return res.status(400).send({ message: "required data not filled" });
     }
-    return await databaseWrapper.remove('player', field, value, res);
+    return await databaseWrapper.remove("player", field, value, res);
   } catch (e) {
-    return res.status(400).send({ message: 'Bad Request', error: e });
+    return res.status(400).send({ message: "Bad Request", error: e });
   }
 };
 
@@ -46,11 +36,28 @@ const updatePlayer = async (req, res) => {
     //todo: data should validate
 
     if (!field || !value || !data) {
-      return res.status(400).send({ message: 'required data not filled' });
+      return res.status(400).send({ message: "required data not filled" });
     }
-    return await databaseWrapper.update('player', field, value, data, res);
+    return await databaseWrapper.update("player", field, value, data, res);
   } catch (e) {
-    return res.status(400).send({ message: 'Bad Request', error: e });
+    return res.status(400).send({ message: "Bad Request", error: e });
+  }
+};
+
+const getPlayerByObjectId = async (req, res) => {
+
+  const { value } = req?.params;
+  console.log("Value: ", value)
+  //check whether a valid object id
+  if (databaseWrapper.isValidObjectId(value)) {
+    const player = await databaseWrapper.read("player", res, ["_id"], [value]);
+    if (player.data == null || player.data.length == 0) {
+      return res.status(400).send("Invalid Player ID. Register as a player first");
+    } else {
+      return res.status(201).send({ message: player.message, data: player.data });
+    }
+  } else {
+    return res.status(400).send("Invalid Player ID");
   }
 };
 
@@ -59,4 +66,5 @@ module.exports = {
   getAllPlayers,
   deleteByField,
   updatePlayer,
+  getPlayerByObjectId
 };
