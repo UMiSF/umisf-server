09bfb54428e82dc709e8cd68d7d806e8ad11dbeb
Supun Dasanayaka <supun.19@cse.mrt.ac.lk>
2023-04-02 14:38:23 +0530

    feature/ add eslint file for the backend
    add eslint file
    fix existing eslint errors in codebase

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const express = require("express");
-require("dotenv").config();
+const express = require('express');
+require('dotenv').config();
 const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const api = process.env.API_URL;
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 //routers
 
 //middleware
 app.use(bodyParser.json());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cors());
-app.options("*", cors());
-app.use(express.static("public"));
+app.options('*', cors());
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3001;
 
 /* ROUTES */
-const playerRouter = require("./router/playerRouter");
-const singleRouter = require("./router/singleRouter");
-const universityRouter = require("./router/universityRouter");
+const playerRouter = require('./router/playerRouter');
+const singleRouter = require('./router/singleRouter');
+const universityRouter = require('./router/universityRouter');
 
-app.use("/player", playerRouter);
-app.use("/single", singleRouter);
-app.use("/university", universityRouter);
+app.use('/player', playerRouter);
+app.use('/single', singleRouter);
+app.use('/university', universityRouter);
 
-app.get(api + "/", (req, res) => {
-  res.send("UMiSF API Started");
+app.get(api + '/', (req, res) => {
+  res.send('UMiSF API Started');
 });
 
 mongoose
@@ -40,12 +40,12 @@ mongoose
     dbName: process.env.DATABASE,
   })
   .then(() => {
-    console.log("Database connection is ready.");
+    console.log('Database connection is ready.');
   })
   .catch((err) => {
     console.log(err);
   });
 
 app.listen(PORT, () => {
-  console.log("Listening to port number " + PORT);
+  console.log('Listening to port number ' + PORT);
 });
